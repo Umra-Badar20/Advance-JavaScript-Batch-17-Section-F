4a44667887763958e561f2fefe87751b159e8bd7
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-01 17:58:26 +0500

    Modules , Map( ) obj

--- class10/main.js
+++ class10/main.js
@@ -12,3 +12,31 @@ sum(2,3)
 multiply(5,5)
 subtract(4,3)
 
+
+
+let myMap = new Map([["a", 1], ["b", 2]])
+myMap.set("fname", "Umra")
+myMap.set(1, "Number")
+myMap.set(true, "Boolean")
+console.log(myMap.get("fname"));
+console.log(myMap.has("fnames"));
+// myMap.delete("fname")
+// myMap.clear()
+
+console.log(typeof myMap, myMap.size,  myMap);
+
+for(let [key, value] of myMap){
+    console.log(key, value);    
+}
+for( let keys of myMap.keys()){
+    console.log("key is :", keys);  
+}
+for( let values of myMap.values()){
+    console.log("value is :", values);  
+}
+
+
+myMap.forEach((values, keys)=>{
+    console.log("key is" , keys);
+    console.log("value is" , values);    
+})
