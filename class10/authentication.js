3754351855fa6bcf4678f12f27392ced006d3a69
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-02 18:03:50 +0500

    modules, Map( ) obj

--- class10/authentication.js
+++ class10/authentication.js
@@ -1,20 +1,27 @@
-let userName = "Umra"
+// let userName = "Umra"
 
-function sum(a,b){
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// export function sub(a,b){
+//     console.log(a-b);
+// }
+// export function multiply(a,b){
+//     console.log(a*b);   
+// }
+
+
+// //default export
+// export default {userName , sum}
+
+// // Named Export
+// // export {sub, multiply} +-
+// // export {multiply}
+
+export let mystudent = "sameen"
+export function sum (a,b){
     console.log(a+b);
 }
-export function sub(a,b){
-    console.log(a-b);
-}
-export function multiply(a,b){
-    console.log(a*b);   
-}
 
-
-//default export
-export default {userName , sum}
-
-// Named Export
-// export {sub, multiply} +-
-// export {multiply}
-
+// export default {mystudent,sum};
+// export {mystudent,sum}
--- class10/main.js
+++ class10/main.js
@@ -1,48 +1,57 @@
-import obj, {sub as subtract, multiply} from "./authentication.js"
-// import {sub as subtract, multiply} from "./authentication.js"
-// console.log(obj.userName);
-// obj.sum(2,4)
+// import obj, {sub as subtract, multiply} from "./authentication.js"
+// // import {sub as subtract, multiply} from "./authentication.js"
+// // console.log(obj.userName);
+// // obj.sum(2,4)
 
-//Destructuring Obj
-let { userName , sum } = obj
-console.log(userName);
-sum(2,3)
+// //Destructuring Obj
+// let { userName , sum } = obj
+// console.log(userName);
+// sum(2,3)
 
-// console.log(sub);
-multiply(5,5)
-subtract(4,3)
-
-
-
-let myMap = new Map([["a", 1], ["b", 2]])
-myMap.set("fname", "Umra")
-myMap.set(1, "Number")
-myMap.set(true, "Boolean")
-console.log(myMap.get("fname"));
-console.log(myMap.has("fnames"));
-// myMap.delete("fname")
-// myMap.clear()
-
-console.log(typeof myMap, myMap.size,  myMap);
-
-for(let [key, value] of myMap){
-    console.log(key, value);    
-}
-for( let keys of myMap.keys()){
-    console.log("key is :", keys);  
-}
-for( let values of myMap.values()){
-    console.log("value is :", values);  
-}
+// // console.log(sub);
+// multiply(5,5)
+// subtract(4,3)
 
 
-myMap.forEach((values, keys)=>{
-    console.log("key is" , keys);
-    console.log("value is" , values);    
-})
 
-let obje = {
-    a:1,
-    b:2
-}
-console.log(Object.entries(obje));
+// let myMap = new Map([["a", 1], ["b", 2]])
+// myMap.set("fname", "Umra")
+// myMap.set(1, "Number")
+// myMap.set(true, "Boolean")
+// console.log(myMap.get("fname"));
+// console.log(myMap.has("fnames"));
+// // myMap.delete("fname")
+// // myMap.clear()
+
+// console.log(typeof myMap, myMap.size,  myMap);
+
+// for(let [key, value] of myMap){
+//     console.log(key, value);    
+// }
+// for( let keys of myMap.keys()){
+//     console.log("key is :", keys);  
+// }
+// for( let values of myMap.values()){
+//     console.log("value is :", values);  
+// }
+
+
+// myMap.forEach((values, keys)=>{
+//     console.log("key is" , keys);
+//     console.log("value is" , values);    
+// })
+
+// let obje = {
+//     a:1,
+//     b:2
+// }
+// console.log(Object.entries(obje));
+
+// import obj from "./authentication.js";
+// let {mystudent,sum}= obj;
+// console.log(mystudent);
+// sum(2,5)
+
+import {mystudent as student,sum as add} from "./authentication.js"
+console.log(student);
+add(2,9)
